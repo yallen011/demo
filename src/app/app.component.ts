0c6e42d00bd06da55eb7f1aaf6dc5b2be89a3bc6
Yvonne Allen <yallen011@gmail.com>
2019-01-11 13:44:29 -0500

    base game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FightService } from './fight/fight.service';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +6,10 @@ import { FightService } from './fight/fight.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'demo';
-  win: boolean;
-  constructor(public fightService: FightService) {
+
+  constructor() {
   }
 
   ngOnInit(): void {
-    console.log(this.fightService.isWinner);
-  //  this.win = this.fightService.health === 0 ? true : false;
-  //  console.log(this.win);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BaneComponent } from './bane/bane.component';
-import { BirdmanComponent } from './birdman/birdman.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BaneComponent,
-    BirdmanComponent
   ],
   imports: [
     BrowserModule
